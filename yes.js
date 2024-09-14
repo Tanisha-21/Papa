6933c12604d2d5daaedf3ad66c138c93f730c944
Tanisha-21 <tanishaagarwal2173@gmail.com>
2024-09-14 15:51:00 +0530

    Add files via upload

---
+++ yes.js
@@ -0,0 +1,4 @@
+function changeText() {
+    var element = document.getElementById('original-content');
+    element.innerHTML = "YES, I know, Happy birthday to you today, papa. You are a good man, an incredible parent and the greatest gift I have ever received. I feel very lucky and very privileged to have had such a wonderful father growing up and, on your special day, I just want you to know how truly grateful I am for you.";
+}
